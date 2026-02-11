27314de0bbffa286f2ac374dbc981a183b039101
Faishal <winterc0ldsye@gmail.com>
2026-02-11 20:22:19 +0700

    chore: setup Netlify deployment and environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZnvbEptSJoe6KVYsBUMv9t9ZvwtJorwQ",
-  authDomain: "todo-ap-a5b25.firebaseapp.com",
-  projectId: "todo-ap-a5b25",
-  storageBucket: "todo-ap-a5b25.firebasestorage.app",
-  messagingSenderId: "1019901906478",
-  appId: "1:1019901906478:web:7560dbc8039397d5f8d9bf",
-  measurementId: "G-WTHWS4RKF1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
